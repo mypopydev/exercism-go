74b7236986d9a6b975c87fa4eb6cb2060ffd8e5c
Jun Zhao <barryjzhao@tencent.com>
2022-05-14 23:40:42 +0800

    Add new solution
    Signed-off-by: Jun Zhao <barryjzhao@tencent.com>

--- accumulate/accumulate.go
+++ accumulate/accumulate.go
@@ -1,1 +1,15 @@
 package accumulate
+
+// Accumulate which, given a collection and an operation to perform on
+// each element of the collection, returns a new collection containing
+// the result of applying that operation to each element of the input
+// collection.
+func Accumulate(list []string, transform func(string) string) []string {
+	var s []string
+
+	for _, entry := range list {
+		s = append(s, transform(entry))
+	}
+
+	return s
+}
