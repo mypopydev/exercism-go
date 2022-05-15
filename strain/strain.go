05b8be838f5f1ff866676b95219e55bbffb58212
Jun Zhao <barryjzhao@tencent.com>
2022-05-15 21:37:58 +0800

    Add new solution
    Signed-off-by: Jun Zhao <barryjzhao@tencent.com>

--- strain/strain.go
+++ strain/strain.go
@@ -5,17 +5,49 @@ type Lists [][]int
 type Strings []string
 
 func (i Ints) Keep(filter func(int) bool) Ints {
-	panic("Please implement the Keep function")
+	var j Ints
+
+	for _, v := range i {
+		if filter(v) {
+			j = append(j, v)
+		}
+	}
+
+	return j
 }
 
 func (i Ints) Discard(filter func(int) bool) Ints {
-	panic("Please implement the Discard function")
+	var j Ints
+
+	for _, v := range i {
+		if filter(v) == false {
+			j = append(j, v)
+		}
+	}
+
+	return j
 }
 
 func (l Lists) Keep(filter func([]int) bool) Lists {
-	panic("Please implement the Keep function")
+	var j Lists
+
+	for _, list := range l {
+		if filter(list) {
+			j = append(j, list)
+		}
+	}
+
+	return j
 }
 
 func (s Strings) Keep(filter func(string) bool) Strings {
-	panic("Please implement the Keep function")
+	var j Strings
+
+	for _, s := range s {
+		if filter(s) {
+			j = append(j, s)
+		}
+	}
+
+	return j
 }
