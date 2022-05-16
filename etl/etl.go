bfb289c30aa6162dcc6ffa7441eabc77f88899c2
Jun Zhao <barryjzhao@tencent.com>
2022-05-16 09:23:11 +0800

    Add new solution
    Signed-off-by: Jun Zhao <barryjzhao@tencent.com>

--- etl/etl.go
+++ etl/etl.go
@@ -1,1 +1,17 @@
 package etl
+
+import "strings"
+
+// Transform( to do the Transform step of an Extract-Transform-Load.
+func Transform(in map[int][]string) map[string]int {
+	ret := make(map[string]int)
+
+	for k, v := range in {
+		for _, s := range v {
+			s = strings.ToLower(s)
+			ret[s] = k
+		}
+	}
+
+	return ret
+}
